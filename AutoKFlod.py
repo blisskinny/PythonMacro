49e3d3af76dce27ef3d219b748e5621f8803ff05
blisskinny <blisskinny@users.noreply.github.com>
2016-02-24 16:29:44 +0800

    Added files via upload

---
+++ AutoKFlod.py
@@ -0,0 +1,22 @@
+#Define target and ID columns:
+target = 'Item_Outlet_Sales'
+IDcol = ['Item_Identifier','Outlet_Identifier']
+from sklearn import cross_validation, metrics
+def modelfit(alg, dtrain, dtest, predictors, target, IDcol, filename):
+    #Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain[target])
+
+    #Predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+
+    #Perform cross-validation:
+    cv_score = cross_validation.cross_val_score(alg, dtrain[predictors], dtrain[target], cv=20, scoring='mean_squared_error')
+    cv_score = np.sqrt(np.abs(cv_score))
+
+    #Print model report:
+    print "\nModel Report"
+    print "RMSE : %.4g" % np.sqrt(metrics.mean_squared_error(dtrain[target].values, dtrain_predictions))
+    print "CV Score : Mean - %.4g | Std - %.4g | Min - %.4g | Max - %.4g" % (np.mean(cv_score),np.std(cv_score),np.min(cv_score),np.max(cv_score))
+
+    #Predict on testing data:
+    dtest[target] = alg.predict(dtest[predictors])
